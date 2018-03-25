eed787c9ceba66dc327f4c9194adf9f6ebcbd6bb
mata <gaoyp_1@sina.com>
2018-03-25 11:02:33 +0800

    disguise spider as a browser

--- wechatspider.py
+++ wechatspider.py
@@ -1,3 +1,14 @@
 """
     This is a wechat spider .
 """
+
+import urllib.request
+
+
+#模拟成浏览器
+headers = ("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0")
+opener = urllib.request.build_opener()
+opener.addheaders = [headers]
+
+#将opener安装为全局
+urllib.request.install_opener(opener)
